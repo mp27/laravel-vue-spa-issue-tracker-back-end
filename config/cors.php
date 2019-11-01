94817e22bb1ae08c77a493629c38e5a1fa259b04
Munteanu Petrisor <munteanu_petrisor2710@yahoo.com>
2019-11-01 19:09:59 +0200

    Add dynamic frontend url

--- config/cors.php
+++ config/cors.php
@@ -13,7 +13,7 @@ return [
     */
 
     'supportsCredentials' => false,
-    'allowedOrigins' => ['*'],
+    'allowedOrigins' => [env('FRONT_APP')],
     'allowedOriginsPatterns' => [],
     'allowedHeaders' => ['*'],
     'allowedMethods' => ['*'],
