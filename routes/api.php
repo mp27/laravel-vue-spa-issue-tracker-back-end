7b822f9d7dc8b3cbbab179dbfeb7c0bfc89e062b
Munteanu Petrisor <munteanu_petrisor2710@yahoo.com>
2019-11-12 18:55:30 +0200

    Add reset-password functionality on API

--- routes/api.php
+++ routes/api.php
@@ -21,5 +21,6 @@ Route::post('/login', 'Api\AuthController@login')->name('api.login');
 Route::post('/register', 'Api\AuthController@register')->name('api.register');
 
 Route::post('/forgot-password', 'Api\ForgotPasswordController@sendResetLinkEmail')->name('api.forgot-password');
+Route::post('/reset-password', 'Api\ResetPasswordController@reset')->name('api.reset-password');
 
 
